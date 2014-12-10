7e3f74a8f4e9b6ca1b9d5f4caaa9e8267ba3c43d
Bruno Veiga <brunoveiga@Brunos-MacBook-Pro.local>
2014-12-10 01:16:57 -0600

    made program mac friendly, and added top down view

--- maze.cpp
+++ maze.cpp
@@ -5,8 +5,6 @@ using namespace std;
 int win_width = 800;
 int win_height = 800;
 
-unsigned int texture[2];
-
 float person_x = 0.0;
 float person_y = -2.0;
 float person_z = -30.0;
@@ -28,80 +26,6 @@ vector<Man> manVector;
 // Global iterators to traverse manVector.
 vector<Man>::iterator manVectorIterator;
 vector<Man>::iterator manVectorAnimationIterator;
-
-/*******************************************************************************
- *  Loading Textures (with error checking)
- *******************************************************************************/
-void fatal(const char* format , ...)
-{
-   va_list args;
-   va_start(args,format);
-   vfprintf(stderr,format,args);
-   va_end(args);
-   exit(1);
-}
-unsigned int LoadTexBMP(char* file)
-{
-   unsigned int   texture;     // Texture name
-   FILE*          f;           // File pointer
-   char           magic[2];    // Image magic
-   unsigned int   dx,dy,size;  // Image dimensions
-   unsigned short nbp,bpp;     // Planes and bits per pixel
-   unsigned char* image;       // Image data
-   int            k;           // Counter
-
-   //  Open file
-   f = fopen(file,"rb");
-   if (!f) fatal("Cannot open file %s\n",file);
-   //  Check image magic
-   if (fread(magic,2,1,f)!=1) fatal("Cannot read magic from %s\n",file);
-   if (strncmp(magic,"BM",2)) fatal("Image magic not BMP in %s\n",file);
-   //  Seek to and read header
-   fseek(f,16,SEEK_CUR);
-   if (fread(&dx ,4,1,f)!=1 || fread(&dy ,4,1,f)!=1 || fread(&nbp,2,1,f)!=1 || fread(&bpp,2,1,f)!=1)
-     fatal("Cannot read header from %s\n",file);
-   //  Check image parameters
-   if (nbp!=1) fatal("%s bit planes is not 1: %d\n",file,nbp);
-   if (bpp!=24) fatal("%s bits per pixel is not 24: %d\n",file,bpp);
-#ifndef GL_VERSION_2_0
-   //  OpenGL 2.0 lifts the restriction that texture size must be a power of two
-   for (k=1;k<dx;k++);
-   if (k!=dx) fatal("%s image width not a power of two: %d\n",file,dx);
-   for (k=1;k<dy;k++);
-   if (k!=dy) fatal("%s image height not a power of two: %d\n",file,dy);
-#endif
-
-   //  Allocate image memory
-   size = 3*dx*dy;
-   image = (unsigned char*) malloc(size);
-   if (!image) fatal("Cannot allocate %d bytes of memory for image %s\n",size,file);
-   //  Seek to and read image
-   fseek(f,24,SEEK_CUR);
-   if (fread(image,size,1,f)!=1) fatal("Error reading data from image %s\n",file);
-   fclose(f);
-   //  Reverse colors (BGR -> RGB)
-   for (k=0;k<size;k+=3)
-   {
-      unsigned char temp = image[k];
-      image[k]   = image[k+2];
-      image[k+2] = temp;
-   }
-
-   //  Generate 2D texture
-   glGenTextures(1,&texture);
-   glBindTexture(GL_TEXTURE_2D,texture);
-   //  Copy image
-   glTexImage2D(GL_TEXTURE_2D,0,3,dx,dy,0,GL_RGB,GL_UNSIGNED_BYTE,image);
-   if (glGetError()) fatal("Error in glTexImage2D %s %dx%d\n",file,dx,dy);
-   //  Scale linearly when image size doesn't match
-   glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_LINEAR);
-   glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_LINEAR);
-
-   //  Free image memory
-   free(image);
-   //  Return texture name
-   return texture;
-}
 
 // Timer function.
 void animate(int value)
@@ -139,9 +63,19 @@ void set_projection() {
 }
 
 void set_viewpoint() {
-	gluLookAt(eyex + modifyx, 7.5, eyez+modifyz,
-		5.0 + modifyx, 5.3, -5.0 + modifyz,
-		0.0, 1.0, 0.0);
+	if(mode == PLAYER_MODE) {
+		gluLookAt(eyex + modifyx, 7.5, eyez+modifyz,
+			5.0 + modifyx, 5.3, -5.0 + modifyz,
+			0.0, 1.0, 0.0);
+	} else if(mode == BIRD_MODE) {
+		gluLookAt(45.0, 50.0, -45.0,
+			5.0 + modifyx, 5.3, -5.0 + modifyz,
+			0.0, 1.0, 0.0);
+	} else if(mode == TOP_DOWN_MODE) {
+		gluLookAt(5.0+modifyx, 50.0, -1.0+modifyz,
+			5.0 + modifyx, 5.3, -5.0 + modifyz,
+			0.0, 1.0, 0.0);
+	}
 }
 
 static void reshape(int width, int height) {
@@ -186,11 +120,13 @@ void keyboard(unsigned char key, int x, int y) {
 			camera.decrementViewDirection();
 			break;
 		case 'p':
-			if(mode == CAMERA_MODE) {
-				mode = PLAYER_MODE;
-			} else {
-				mode = CAMERA_MODE;
-			}
+			mode = PLAYER_MODE;
+			break;
+		case 'b':
+			mode = BIRD_MODE;
+			break;
+		case 't':
+			mode = TOP_DOWN_MODE;
 			break;
 		default:
 			break;
@@ -200,90 +136,71 @@ void keyboard(unsigned char key, int x, int y) {
 }
 
 void keyboard2(int key, int x, int y) {
-	if(mode == PLAYER_MODE) {
-		switch(key) {
-			case GLUT_KEY_LEFT:
-				camera.turnLeft();
-				direction = manVectorIterator->turnLeft();
-				if(direction == LEFT){
-					eyex = 9.0;
-					eyez = -5.0;
-				}
-				else if(direction == DOWN){
-					eyex = 5.0;
-					eyez = -9.0;
-				}
-				else if(direction == RIGHT){
-					eyex = 1.0;
-					eyez = -5.0;
-				}
-				else{
-					eyex = 5.0;
-					eyez = -1.0;
-				}
-				break;
-			case GLUT_KEY_RIGHT:
-				camera.turnRight();
-				direction = manVectorIterator->turnRight();
-				if(direction == RIGHT){
-					eyex = 1.0;
-					eyez = -5.0;
-				}
-				else if(direction == DOWN){
-					eyex = 5.0;
-					eyez = -9.0;
-				}
-				else if(direction == LEFT){
-					eyex = 9.0;
-					eyez = -5.0;
-				}
-				else{
-					eyex = 5.0;
-					eyez = -1.0;
-				}
-				break;
-			case GLUT_KEY_UP:
-				direction = manVectorIterator->incrementMove();
-				if(direction == LEFT)
-					modifyx -= SPEED;
-				else if(direction == DOWN)
-					modifyz += SPEED;
-				else if(direction == RIGHT)
-					modifyx += SPEED;
-				else
-					modifyz -= SPEED;
-				break;
-			case GLUT_KEY_DOWN:
-				direction = manVectorIterator->decrementMove();
-				if(direction == LEFT)
-					modifyx += SPEED;
-				else if(direction == DOWN)
-					modifyz -= SPEED;
-				else if(direction == RIGHT)
-					modifyx -= SPEED;
-				else
-					modifyz += SPEED;
-				break;
-			default:
-				break;
-		}
-	} else {
-		switch(key) {
-			case GLUT_KEY_LEFT:
-				person_x -= 1;
-				break;
-			case GLUT_KEY_RIGHT:
-				person_x += 1;
-				break;
-			case GLUT_KEY_UP:
-				person_y += 1;
-				break;
-			case GLUT_KEY_DOWN:
-				person_y -= 1;
-				break;
-			default:
-				break;
-		}
+	switch(key) {
+		case GLUT_KEY_LEFT:
+			camera.turnLeft();
+			direction = manVectorIterator->turnLeft();
+			if(direction == LEFT){
+				eyex = 9.0;
+				eyez = -5.0;
+			}
+			else if(direction == DOWN){
+				eyex = 5.0;
+				eyez = -9.0;
+			}
+			else if(direction == RIGHT){
+				eyex = 1.0;
+				eyez = -5.0;
+			}
+			else{
+				eyex = 5.0;
+				eyez = -1.0;
+			}
+			break;
+		case GLUT_KEY_RIGHT:
+			camera.turnRight();
+			direction = manVectorIterator->turnRight();
+			if(direction == RIGHT){
+				eyex = 1.0;
+				eyez = -5.0;
+			}
+			else if(direction == DOWN){
+				eyex = 5.0;
+				eyez = -9.0;
+			}
+			else if(direction == LEFT){
+				eyex = 9.0;
+				eyez = -5.0;
+			}
+			else{
+				eyex = 5.0;
+				eyez = -1.0;
+			}
+			break;
+		case GLUT_KEY_UP:
+			direction = manVectorIterator->incrementMove();
+			if(direction == LEFT)
+				modifyx -= SPEED;
+			else if(direction == DOWN)
+				modifyz += SPEED;
+			else if(direction == RIGHT)
+				modifyx += SPEED;
+			else
+				modifyz -= SPEED;
+			break;
+		case GLUT_KEY_DOWN:
+			direction = manVectorIterator->decrementMove();
+			if(direction == LEFT)
+				modifyx += SPEED;
+			else if(direction == DOWN)
+				modifyz -= SPEED;
+			else if(direction == RIGHT)
+				modifyx -= SPEED;
+			else
+				modifyz += SPEED;
+			break;
+		default:
+			break;
 	}
 
 	glutPostRedisplay();
@@ -296,9 +213,6 @@ int main(int argc, char** argv) {
 	glutInitWindowPosition(100, 100);
 	glutCreateWindow( "MAZE" );
 	Init();
-
-	string name = "textures/wall_texture800x800.bmp";
-	texture[0] = LoadTexBMP(strdup(name.c_str()));
 
 	// Set Callback Functions
 	glutDisplayFunc(display);
--- maze.h
+++ maze.h
@@ -22,14 +22,15 @@
 using namespace std;
 
 #define PI 3.14159265
-#define CAMERA_MODE 1
-#define PLAYER_MODE 2
+#define PLAYER_MODE 1
+#define BIRD_MODE 2
+#define TOP_DOWN_MODE 3
 
-#define UP 3
-#define RIGHT 4
-#define DOWN 5
-#define LEFT 6
-#define SPEED 0.2
+#define UP 4
+#define RIGHT 5
+#define DOWN 6
+#define LEFT 7
+#define SPEED 0.9
 
 // Man Globals.
 static float highlightColor[3] = {0.0, 0.0, 0.0}; // Emphasize color.
@@ -40,9 +41,86 @@ static int animateMode = 0; // In animation mode?
 static int animationPeriod = 1000; // Time interval between frames.
 static ofstream outFile; // File to write configurations data.
 
+// Texture
+unsigned int texture[5];
+
+/*******************************************************************************
+ *  1. Loading Textures (with error checking)
+ *******************************************************************************/
+void fatal(const char* format , ...)
+{
+   va_list args;
+   va_start(args,format);
+   vfprintf(stderr,format,args);
+   va_end(args);
+   exit(1);
+}
+unsigned int LoadTexBMP(char* file)
+{
+   unsigned int   texture;     // Texture name
+   FILE*          f;           // File pointer
+   char           magic[2];    // Image magic
+   unsigned int   dx,dy,size;  // Image dimensions
+   unsigned short nbp,bpp;     // Planes and bits per pixel
+   unsigned char* image;       // Image data
+   int            k;           // Counter
+
+   //  Open file
+   f = fopen(file,"rb");
+   if (!f) fatal("Cannot open file %s\n",file);
+   //  Check image magic
+   if (fread(magic,2,1,f)!=1) fatal("Cannot read magic from %s\n",file);
+   if (strncmp(magic,"BM",2)) fatal("Image magic not BMP in %s\n",file);
+   //  Seek to and read header
+   fseek(f,16,SEEK_CUR);
+   if (fread(&dx ,4,1,f)!=1 || fread(&dy ,4,1,f)!=1 || fread(&nbp,2,1,f)!=1 || fread(&bpp,2,1,f)!=1)
+     fatal("Cannot read header from %s\n",file);
+   //  Check image parameters
+   if (nbp!=1) fatal("%s bit planes is not 1: %d\n",file,nbp);
+   if (bpp!=24) fatal("%s bits per pixel is not 24: %d\n",file,bpp);
+#ifndef GL_VERSION_2_0
+   //  OpenGL 2.0 lifts the restriction that texture size must be a power of two
+   for (k=1;k<dx;k++);
+   if (k!=dx) fatal("%s image width not a power of two: %d\n",file,dx);
+   for (k=1;k<dy;k++);
+   if (k!=dy) fatal("%s image height not a power of two: %d\n",file,dy);
+#endif
+
+   //  Allocate image memory
+   size = 3*dx*dy;
+   image = (unsigned char*) malloc(size);
+   if (!image) fatal("Cannot allocate %d bytes of memory for image %s\n",size,file);
+   //  Seek to and read image
+   fseek(f,24,SEEK_CUR);
+   if (fread(image,size,1,f)!=1) fatal("Error reading data from image %s\n",file);
+   fclose(f);
+   //  Reverse colors (BGR -> RGB)
+   for (k=0;k<size;k+=3)
+   {
+      unsigned char temp = image[k];
+      image[k]   = image[k+2];
+      image[k+2] = temp;
+   }
+
+   //  Generate 2D texture
+   glGenTextures(1,&texture);
+   glBindTexture(GL_TEXTURE_2D,texture);
+   //  Copy image
+   glTexImage2D(GL_TEXTURE_2D,0,3,dx,dy,0,GL_RGB,GL_UNSIGNED_BYTE,image);
+   if (glGetError()) fatal("Error in glTexImage2D %s %dx%d\n",file,dx,dy);
+   //  Scale linearly when image size doesn't match
+   glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_LINEAR);
+   glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_LINEAR);
+
+   //  Free image memory
+   free(image);
+   //  Return texture name
+   return texture;
+}
+
 
 /*******************************************************************
- *	CAMERA STUFF
+ *	2. CAMERA STUFF
 ********************************************************************/
 class Camera
 {
@@ -132,7 +210,7 @@ void Camera::decrementViewDirection()
 }
 
 /*******************************************************************
- *	MAN STUFF
+ *	3. MAN STUFF
 ********************************************************************/
 class Man
 {
@@ -508,22 +586,36 @@ void Man::writeData()
 
 
 /*******************************************************************************
- *  DRAWING STUFF
+ *  4. DRAWING STUFF
  *******************************************************************************/
 void draw_fountain() {
+	GLUquadricObj *obj = gluNewQuadric();	// mac
+
 	glPushMatrix();
 		glColor3f(0.8, 0.8, 0.8);
 		glTranslatef(45.0, 1.0, -45.0);
 		glRotatef(90.0, 1.0, 0.0, 0.0);
-		glutSolidCylinder(5.0, 1.0, 20, 20);
+		// mac
+		gluCylinder(obj, 5.0, 5.0, 1.0, 20, 20);
+		gluDisk(obj, 0.0, 5.0, 20, 20);
+		// linux
+		// glutSolidCylinder(5.0, 1.0, 20, 20);
 
 		glColor3f(1.0, 0.0, 0.0);
 		glTranslatef(0.0, 0.0, -4.0);
-		glutSolidCylinder(2.5, 0.5, 20, 20);
+		// mac
+		gluCylinder(obj, 2.5, 2.5, 0.5, 20, 20);
+		gluDisk(obj, 0.0, 2.5, 20, 20);
+		// linux
+		// glutSolidCylinder(2.5, 0.5, 20, 20);	
 
 		glColor3f(0.4, 0.4, 0.4);
 		glTranslatef(0.0, 0.0, -2.0);
-		glutSolidCylinder(0.35, 20.0, 20, 20);
+		// mac
+		gluCylinder(obj, 0.35, 0.35, 20.0, 20, 20);
+		gluDisk(obj, 0.0, 0.35, 20, 20);
+		// linux
+		// glutSolidCylinder(0.35, 20.0, 20, 20);
 	glPopMatrix();
 }
 
@@ -564,6 +656,9 @@ void draw_wall(){
 
 void draw_maze() {
 	draw_fountain();
+
+	string name = "textures/wall_texture800x800.bmp";
+	texture[0] = LoadTexBMP(strdup(name.c_str()));
 
 	int i;
 	glPushMatrix();
@@ -847,16 +942,22 @@ void draw_maze() {
 		draw_wall();
 	glPopMatrix();
 
-	// Drawing Maze Floor 
+	// Drawing Maze Floor
+	name = "textures/floor_texture800x800.bmp";
+	texture[1] = LoadTexBMP(strdup(name.c_str()));
+	glEnable(GL_TEXTURE_2D);
 	glBegin(GL_QUADS);
 		glColor3f(0.0, 0.4, 0.0);
-		glVertex3f(0.0, 0.0, 0.0);
-	   	glVertex3f(90.0, 0.0, 0.0);
-	   	glVertex3f(90.0, 0.0, -90.0);
-	   	glVertex3f(0.0, 0.0, -90.0);
+		glTexCoord2f(0.0, 0.9); glVertex3f(0.0, 0.0, 0.0);
+	   	glTexCoord2f(1.0, 0.9); glVertex3f(90.0, 0.0, 0.0);
+	   	glTexCoord2f(1.0, 1.0); glVertex3f(90.0, 0.0, -90.0);
+	   	glTexCoord2f(0.0, 1.0); glVertex3f(0.0, 0.0, -90.0);
 	glEnd();
+	glDisable(GL_TEXTURE_2D);
 
 	// Drawing Maze Walls
+	name = "textures/wall_texture800x800.bmp";
+	texture[0] = LoadTexBMP(strdup(name.c_str()));
 	for(i = 0; i < 9; ++i) {
 		draw_wall();
 		glTranslatef(10.0, 0.0, 0.0);
